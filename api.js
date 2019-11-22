a0f7d52a0905100c6efe87633acc2fcbb2f41154
cppchriscpp <admin@cpprograms.net>
2019-11-21 22:10:25 -0500

    SRAM editing functions

--- api.js
+++ api.js
@@ -8,3 +8,13 @@ var Api = {};
 Api.getSram = function() {
     return cpuMemory.subarray(0x6000, 0x8000);
 };
+
+Api.setSramValue = function(addr, value) {
+    cpuMemory.set[0x6000+addr] = value;
+};
+
+Api.setSramValues = function(addr, values) {
+    for (var i = 0; i < values.length; i++) {
+        cpuMemory[0x6000+addr+i] = values[i];
+    }
+};
--- dist/emulatorscript.js
+++ dist/emulatorscript.js
@@ -3052,6 +3052,16 @@ Introduces a very simple api off the `window.emu` object. Allows for getting/set
 var Api = {};
 Api.getSram = function() {
     return cpuMemory.subarray(0x6000, 0x8000);
+};
+
+Api.setSramValue = function(addr, value) {
+    cpuMemory.set[0x6000+addr] = value;
+};
+
+Api.setSramValues = function(addr, values) {
+    for (var i = 0; i < values.length; i++) {
+        cpuMemory[0x6000+addr+i] = values[i];
+    }
 };		
 	var prgData;
 	var chrData;
